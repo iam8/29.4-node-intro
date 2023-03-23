f1890026cabcd99553b55366b2e50bacc5a7ae35
alexm <alex-m@outlook.com>
2023-03-22 19:00:24 -0700

    Copy code from step2.js

--- step3.js
+++ step3.js
@@ -1,2 +1,50 @@
 // Ioana A Mititean
 // Exercise 29.4 - Node Intro
+
+const fs = require('fs');
+const axios = require("axios");
+
+
+function cat(path) {
+
+    fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+
+        console.log(data);
+    })
+}
+
+
+async function webCat(url) {
+
+    let htmlData;
+
+    try {
+        htmlData = await axios.get(url);
+    } catch (error) {
+        console.log(error.cause);
+        process.exit(1);
+    }
+
+    console.log(htmlData.data);
+}
+
+
+const pathOrUrl = process.argv[2];
+
+if (pathOrUrl) {
+    let isValidUrl;
+
+    // Determine if arg is a valid URL (pattern)
+    try {
+        new URL(pathOrUrl);
+        isValidUrl = true;
+    } catch (error) {
+        isValidUrl = false;
+    }
+
+    isValidUrl ? webCat(pathOrUrl) : cat(pathOrUrl);
+}
