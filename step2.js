bd14ee399ea2146242a52fc579ea2be96ffba1f3
alexm <alex-m@outlook.com>
2023-03-22 18:09:18 -0700

    Copy code from step1.js

--- step2.js
+++ step2.js
@@ -1,2 +1,24 @@
 // Ioana A Mititean
 // Exercise 29.4 - Node Intro
+
+const fs = require('fs');
+
+
+function cat(path) {
+
+    fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+
+        console.log(data);
+    })
+}
+
+
+const path = process.argv[2];
+
+if (path) {
+    cat(path);
+}
