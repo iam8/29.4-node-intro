0ca8cd9b357023b96d610dbead2f81b4e462dc86
alexm <alex-m@outlook.com>
2023-03-22 22:38:32 -0700

    Add temporary comments with tests to run; write functions for step 3 cat and catWeb (with file writing); add helper function for checking URL validity; add code to deal with all possible command line args

--- step3.js
+++ step3.js
@@ -18,6 +18,24 @@ function cat(path) {
 }
 
 
+function catWrite(path, outFilePath) {
+
+    fs.readFile(path, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+
+        fs.writeFile(outFilePath, data, "utf8", function(err) {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+        })
+    })
+}
+
+
 async function webCat(url) {
 
     let htmlData;
@@ -33,18 +51,79 @@ async function webCat(url) {
 }
 
 
-const pathOrUrl = process.argv[2];
+async function webCatWrite(url, outFilePath) {
 
-if (pathOrUrl) {
-    let isValidUrl;
+    let htmlData;
 
-    // Determine if arg is a valid URL (pattern)
     try {
-        new URL(pathOrUrl);
-        isValidUrl = true;
+        htmlData = await axios.get(url);
     } catch (error) {
-        isValidUrl = false;
+        console.log(error.cause);
+        process.exit(1);
     }
 
-    isValidUrl ? webCat(pathOrUrl) : cat(pathOrUrl);
+    fs.writeFile(outFilePath, htmlData.data, "utf8", function(err) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+    })
 }
+
+
+function isValidUrl(string) {
+
+    // Determine if the given string is a valid URL pattern
+
+    try {
+        new URL(string);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+
+// Retrieve relevant args from command line
+const args = process.argv.slice(2);
+
+let outFile;
+let pathOrUrl;
+
+if (args.length > 0) {
+
+    if (args[0] == "--out") {
+        outFile = args[1];
+        pathOrUrl = args[2];
+
+        if (outFile && pathOrUrl) {
+
+            // Write to output file
+            isValidUrl(pathOrUrl) ? webCatWrite(pathOrUrl, outFile) : catWrite(pathOrUrl, outFile);
+        }
+    } else {
+
+        pathOrUrl = args[0];
+
+        // Just display output (no file writing)
+        isValidUrl(pathOrUrl) ? webCat(pathOrUrl) : cat(pathOrUrl);
+    }
+}
+
+// Tests
+
+// Failures
+// node step3.js
+// node step3.js --out
+// node step3.js --out output.txt
+// node step3.js --out output.txt fhsdjh
+// node step3.js --out outputweb2.txt https://googlehsjdhsj.com
+// node step3.js onefsdfs.txt
+// node step3.js https://googleshdjshdjs.com
+
+
+// Successes
+// node step3.js --out output.txt one.txt
+// node step3.js --out outputweb.txt https://google.com
+// node step3.js one.txt
+// node step3.js https://google.com
